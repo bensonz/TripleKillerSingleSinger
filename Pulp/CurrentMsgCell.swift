cf24588f7586d09b64c1c2b509998d0576271284
bensonz <yaxiangz@andrew.cmu.edu>
2014-09-27 02:07:50 -0400

    updated cells

---
+++ Pulp/CurrentMsgCell.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentMsgCell.swift
+//  Pulp
+//
+//  Created by Bz on 9/27/14.
+//  Copyright (c) 2014 Antonio Ono. All rights reserved.
+//
+
+import UIKit
+
+class CurrentMsgCell : UICollectionViewCell{
+    
+}
---
+++ Pulp/SavedMsgCell.swift
@@ -0,0 +1,13 @@
+//
+//  SavedMsgCell.swift
+//  Pulp
+//
+//  Created by Bz on 9/27/14.
+//  Copyright (c) 2014 Antonio Ono. All rights reserved.
+//
+
+import UIKit
+
+class SavedMsgCell : UICollectionViewCell{
+    
+}
